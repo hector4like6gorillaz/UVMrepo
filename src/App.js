cf0ed141bc15e3626a26160ab363c5f7060fbb5b
hectorbalan <balan.huchin.3@gmail.com>
2021-11-05 23:51:33 -0600

    add comentary

--- src/App.js
+++ src/App.js
@@ -27,3 +27,13 @@ function App() {
 }
 
 export default App;
+
+/**
+ echo "# UVMrepo" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/hector4like6gorillaz/UVMrepo.git
+git push -u origin main
+ */
