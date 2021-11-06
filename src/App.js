3e021c66ce42c82f6fb6b1dd5b1943dc266ac0a5
hectorbalan <balan.huchin.3@gmail.com>
2021-11-06 11:40:05 -0600

    cambios en vivo

--- src/App.js
+++ src/App.js
@@ -2,23 +2,38 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
-
-
 function App() {
   const [valor, setvalor] = useState(""); //este es un hook, llamdo useState
-
 
   //este un comentario
   //de ejemplo para visualizar
 
- 
+  let arregloEjemplo = [1, 2, "jhk7", "uj", "dsadsd", "sad", "hector"];
+  let verdadero = false;
+  let otroFalseverdadero = true;
+  let objetoEjemplo = {
+    nombre: "Hector",
+    apellido: "Balan",
+    edad: 29,
+    arreglo: [1, 2, 3, "valor", "6"],
+  };
 
-  let nombre = "hector";
-  function ImprimirNombre(){
-    console.log("estoy en nombre", nombre)
+  for (let contador = 0; contador < arregloEjemplo.length ; contador = contador + 1) 
+  {
+    console.log(arregloEjemplo.length)
+    console.log("valor de contador: ", arregloEjemplo[contador]);
   }
 
- ImprimirNombre();
+  console.log(objetoEjemplo["nombre"], "soy el objeto de ejemplo");
+  //console.log(arregloEjemplo, "soy el arreglo");
+  //console.log(arregloEjemplo[0] + arregloEjemplo[1], "soy el arreglo");
+
+  let nombre = "hector";
+  function ImprimirNombre() {
+    console.log("estoy en nombre", nombre);
+  }
+
+  ImprimirNombre();
 
   function mensaje() {
     //esta es una funcion
@@ -31,7 +46,6 @@ function App() {
   }
 
   return (
-    
     <div className="App">
       <header className="App-header">
         <h3>Programacion estructurada</h3>
@@ -39,9 +53,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {objetoEjemplo.nombre}
+        {objetoEjemplo.apellido}
         <input
           placeholder="escribe tu nombre"
-          onChange={(e)=> change(e)}
+          onChange={(e) => change(e)}
           value={valor}
         />
         <button onClick={() => mensaje()}>Presioname!!!! porfavor</button>
