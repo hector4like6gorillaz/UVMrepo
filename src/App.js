ce73476201f85d79c65f3c9c5fd588957e56f91f
hectorbalan <balan.huchin.3@gmail.com>
2021-11-05 23:46:29 -0600

    add h3 and text

--- src/App.js
+++ src/App.js
@@ -5,9 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h3>Programacion estructurada</h3>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Si me descargaste es porque ya sabes para que sirve git hub.
         </p>
         <a
           className="App-link"
